236fabdf539c5b4d7b8fdd7d31d0e3a0a28f8eb6
Abduttayyeb <absking786@gmail.com>
2020-06-21 20:32:24 +0530

    Basic Pr

---
+++ Bas.py
@@ -0,0 +1,7 @@
+char=input("Enter A Character or Number : ")
+if char>="A" and char<="Z":
+    print(char,"Is An Alphabet Of Uppercase") 
+elif char>="a" and char<="z":
+    print(char,"Is An Alphabet Of Lowercase")
+else:
+    print(char,"Is A Number")
